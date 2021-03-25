d2d3c4d295309a68fe60e53d543e1b3f92ebf595
younghoon.choi <c_yh0327@naver.com>
2021-03-25 12:28:26 +0900

    [feat] update Notice close #28

--- src/graphql/mergeSchema.js
+++ src/graphql/mergeSchema.js
@@ -8,7 +8,6 @@ const path = require('path');
 
 const allTypes = loadFilesSync(path.join(__dirname, '../api/**/*.graphql'));
 const allResolvers = loadFilesSync(path.join(__dirname, '../api/**/*.resolvers.js'));
-
 const schema = makeExecutableSchema({
     typeDefs: mergeTypeDefs(allTypes),
     resolvers: mergeResolvers(allResolvers),
