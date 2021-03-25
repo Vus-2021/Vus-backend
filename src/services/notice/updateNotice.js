d267c724ebdf8903214a870ed39dd7e622da4164
younghoon.choi <c_yh0327@naver.com>
2021-03-25 13:57:31 +0900

    [feat] delete route close #22

--- src/services/notice/updateNotice.js
+++ src/services/notice/updateNotice.js
@@ -3,7 +3,7 @@ const vus = require('../../model/vus');
 const updateNotice = async ({ primaryKey, updateItem }) => {
     try {
         await vus.update(primaryKey, { $SET: updateItem });
-        return { success: true, message: 'success update', code: '204' };
+        return { success: true, message: 'success update', code: 204 };
     } catch (error) {
         return { success: false, message: error.message, code: 500 };
     }
--- src/services/route/getBusInfoBybusId.js
+++ src/services/route/getBusInfoBybusId.js
@@ -8,6 +8,7 @@ const getBusInfo = async ({ partitionKey, sortKey }) => {
             .where('sortKey')
             .beginsWith(sortKey)
             .exec();
+
         return { success: true, message: 'success get bus info', code: 200, data: bus };
     } catch (error) {
         return { success: false, message: 'failed get bus info', code: 500 };
