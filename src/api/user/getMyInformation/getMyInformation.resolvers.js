7253b3b6308326b3730fc603d12a5b46c9147087
younghoon.choi <c_yh0327@naver.com>
2021-03-30 13:53:24 +0900

    [refactor] myInformation 추가

--- src/api/user/getMyInformation/getMyInformation.resolvers.js
+++ src/api/user/getMyInformation/getMyInformation.resolvers.js
@@ -1,14 +1,36 @@
+const getUserByPk = require('../../../services/user/getUserByPk');
+
 const resolvers = {
     Query: {
-        getMyInformation: (parent, args, context) => {
+        getMyInformation: async (parent, args, context) => {
             if (!context.user) return { success: false, message: context.message, code: 400 };
+            try {
+                let { data } = await getUserByPk({ partitionKey: context.user.userId });
 
-            return {
-                success: true,
-                message: 'success get my information',
-                code: 200,
-                data: context.user,
-            };
+                data = data
+                    .filter((item) => item.sortKey !== '#user')
+                    .filter((item) => item.state === 'pending' || item.state === 'fulfilled')
+                    .map((dataItem) => {
+                        return {
+                            partitionKey: dataItem.partitionKey,
+                            sortKey: dataItem.sortKey,
+                            routes: dataItem.gsiSortKey,
+                            busId: dataItem.busId,
+                            month: dataItem.sortKey.split('#')[2],
+                        };
+                    });
+                const obj = context.user;
+                obj.routeInfo = data;
+
+                return {
+                    success: true,
+                    message: 'success get my information',
+                    code: 200,
+                    data: obj,
+                };
+            } catch (error) {
+                return { success: false, message: error.message, code: 500 };
+            }
         },
     },
 };
