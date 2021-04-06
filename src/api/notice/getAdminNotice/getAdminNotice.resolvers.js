6a7fc505be71a8f8644cfa7421ef6d5605c13257
younghoon.choi <c_yh0327@naver.com>
2021-04-06 13:37:53 +0900

    [feat] get admin notice에 limit 추가함

--- src/api/notice/getAdminNotice/getAdminNotice.resolvers.js
+++ src/api/notice/getAdminNotice/getAdminNotice.resolvers.js
@@ -4,16 +4,17 @@ const searchValidator = require('../../../modules/searchValidator');
 const resolvers = {
     Query: {
         getAdminNotice: async (_, args) => {
-            const { isMatched, notice, name, content } = {
+            const { isMatched, notice, name, content, limit } = {
                 notice: args.notice,
                 name: args.name,
                 content: args.content,
                 isMatched: args.isMatched || false,
+                limit: args.limit,
             };
             let condition = searchValidator({ isMatched, notice, name, content });
 
             try {
-                const { success, message, code, data } = await getAdminNotice({
+                let { success, message, code, data } = await getAdminNotice({
                     sortKey: '#notice',
                     index: 'sk-index',
                     noticeType: 'ADMIN',
@@ -24,7 +25,9 @@ const resolvers = {
                     item.createdAt = item.gsiSortKey.split('#')[2];
                     item.author = item.name;
                 });
-
+                if (limit) {
+                    data = data.slice(0, limit);
+                }
                 return { success, message, code, data };
             } catch (error) {
                 return { success: false, message: error.message, code: 500 };
