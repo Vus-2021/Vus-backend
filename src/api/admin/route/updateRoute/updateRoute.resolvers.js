38cdc5e7d5813f4611da66b4f5a4956b58bdbfa1
younghoon.choi <c_yh0327@naver.com>
2021-04-07 10:30:11 +0900

    [fix] update route info 1. detail까지 함께 업데이트 시킴

--- src/api/admin/route/updateRoute/updateRoute.resolvers.js
+++ src/api/admin/route/updateRoute/updateRoute.resolvers.js
@@ -1,8 +1,10 @@
-const updateDetailRoute = require('../../../../services/route/updateDetailRoute');
+const updateRouteInfoDetail = require('../../../../services/route/updateRouteInfoDetail');
+const getRouteById = require('../../../../services/route/getRouteById');
+const getDetailRoutesByRoute = require('../../../../services/route/getDetailRoutesByRoute');
 const uploadS3 = require('../../../../modules/s3');
 
 /**
- * TODO onDelete ..
+ * TODO onUpdate
  */
 const resolvers = {
     Mutation: {
@@ -15,19 +17,44 @@ const resolvers = {
                 return { success: false, message: 'access denied', code: 403 };
             }
             let updateItem;
+            updateItem = { gsiSortKey: route, busNumber, limitCount, driver };
+            /**
+             *  partitionKey로 route를 조회해서 달라졌으면 트리거, 아니면 이대로 진행
+             */
 
             try {
-                if (!file) {
-                    updateItem = { route, busNumber, limitCount, driver };
-                } else {
+                const thisRoute = (await getRouteById({ partitionKey, sortKey: '#info' })).route;
+                let detailList;
+                if (route !== thisRoute.gsiSortKey) {
+                    const details = await getDetailRoutesByRoute({
+                        sortKey: '#detail',
+                        route: thisRoute.gsiSortKey,
+                        index: 'sk-index',
+                    });
+                    detailList = details.routeDetails.map((item) => {
+                        return {
+                            partitionKey: item.partitionKey,
+                            sortKey: '#detail',
+                            route: route,
+                            gsiSortKey: item.gsiSortKey,
+                            location: item.location,
+                            lat: item.lat,
+                            long: item.long,
+                        };
+                    });
+                }
+
+                if (file) {
                     const { createReadStream, filename } = await file;
                     const fileStream = createReadStream();
                     const fileInfo = await uploadS3({ fileStream, filename });
                     updateItem = Object.assign(updateItem, { imageUrl: fileInfo.Location });
                 }
-                const { success, message, code } = await updateDetailRoute({
+
+                const { success, message, code } = await updateRouteInfoDetail({
                     primaryKey: { partitionKey, sortKey: '#info' },
                     updateItem,
+                    detailList,
                 });
 
                 return { success, message, code };
