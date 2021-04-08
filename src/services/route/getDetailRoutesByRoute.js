ba9a742dbd7064187a6fbc585d9b61849eea877f
younghoon.choi <c_yh0327@naver.com>
2021-04-08 14:17:48 +0900

    [fix] add get locationIndex

--- src/services/route/getDetailRoutesByRoute.js
+++ src/services/route/getDetailRoutesByRoute.js
@@ -1,16 +1,28 @@
 const vus = require('../../model/vus');
 
-const getDetailRoutesByRoute = async ({ sortKey, route, index }) => {
+const getDetailRoutesByRoute = async ({ sortKey, route, index, condition }) => {
     try {
-        const routeDetails = await vus
-            .query('sortKey')
-            .eq(sortKey)
-            .where('route')
-            .eq(route)
-            .sort('ascending')
-            .using(index)
-            .exec();
-
+        let routeDetails;
+        if (!condition) {
+            routeDetails = await vus
+                .query('sortKey')
+                .eq(sortKey)
+                .where('route')
+                .eq(route)
+                .sort('ascending')
+                .using(index)
+                .exec();
+        } else {
+            routeDetails = await vus
+                .query(condition)
+                .where('sortKey')
+                .eq(sortKey)
+                .where('route')
+                .eq(route)
+                .sort('ascending')
+                .using(index)
+                .exec();
+        }
         return { success: true, message: 'getRoute', code: 201, routeDetails };
     } catch (error) {
         return { success: false, message: error.message, code: 500 };
