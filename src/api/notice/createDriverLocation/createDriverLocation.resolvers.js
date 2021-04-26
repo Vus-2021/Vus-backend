309adb9722ca463cfa97e9b5555d3baefe715f84
younghoon.choi <c_yh0327@naver.com>
2021-04-26 17:41:43 +0900

    [feat] createDriverLocation SNS topic publish

--- src/api/notice/createDriverLocation/createDriverLocation.resolvers.js
+++ src/api/notice/createDriverLocation/createDriverLocation.resolvers.js
@@ -1,11 +1,14 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'ap-northeast-2' });
+
 const { transaction } = require('../../../services');
 const dateNow = require('../../../modules/dateNow');
 
 const resolvers = {
     Mutation: {
         createDriverLocation: async (parent, args) => {
-            const { preKey, destinationKey, locationIndex } = args.input;
-
+            const { preKey, destinationKey, locationIndex, route } = args.input;
             const updatedAt = dateNow();
             const Update = [
                 {
@@ -30,6 +33,28 @@ const resolvers = {
                     Update,
                     tableName: process.env.TABLE_NAME,
                 });
+
+                if (success) {
+                    const params = {
+                        Message: route,
+                        TopicArn: process.env.AWS_SNS_ARN,
+                    };
+
+                    const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' })
+                        .publish(params)
+                        .promise();
+
+                    publishTextPromise
+                        .then((data) => {
+                            console.log(
+                                `Message ${params.Message} sent to the topic ${params.TopicArn}`
+                            );
+                            console.log('MessageID is ' + data.MessageId);
+                        })
+                        .catch((err) => {
+                            console.error('err', err);
+                        });
+                }
                 return { success, message, code };
             } catch (error) {
                 return { success: false, message: 'asda', code: 500 };
